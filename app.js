a2e501ddbafdc34e6c1115c7f91e4c61e84af017
Thang Le Dinh <thangld2407@gmail.com>
2022-04-01 14:17:21 +0700

    hot fix

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "Accept",
+    "X-Requested-With",
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Request-Method",
+    "Access-Control-Request-Headers"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
