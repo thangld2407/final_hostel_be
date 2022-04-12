fb1422ecfa8a12bd9f6bb50d6faa05ceb51c1ec4
Thang Le Dinh <thangld2407@gmail.com>
2022-04-12 14:58:17 +0700

    add field date area

--- model/area.js
+++ model/area.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
-const Area = mongoose.Schema({
-  area_name: {
-    type: String,
-    require: true,
+const Area = mongoose.Schema(
+  {
+    area_name: {
+      type: String,
+      require: true,
+    },
   },
-});
+  {
+    timestamps: {
+      createdAt: "createAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
 module.exports = mongoose.model("Area", Area);
